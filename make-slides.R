964a222892b1bf4a2cdf4479d4864969f72304c1
Thinh Ong <34956551+thinhong@users.noreply.github.com>
2026-04-24 23:56:26 +0700

    update d3 interactive illustrations

--- make-slides.R
+++
@@ -1,7 +0,0 @@
-library(quarto)
-
-# Render slides
-quarto_render(input = "slides/", output_format = "revealjs")
-
-# Delete *_files folders in the slides folder
-unlink("slides/*_files/", recursive = T)
